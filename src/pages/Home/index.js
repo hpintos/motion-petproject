f42dd7ef90f37bb5240857f06fd180bbfb70ebd0
Hernan Pintos <hernan.pintos@adevinta.com>
2022-08-26 11:20:46 +0200

    Another commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,9 @@
-import Helmet from 'react-helmet'
-
 import ReactSpringWrapper from '../../components/react-spring/index.js'
 
 export default function HomePage() {
   return (
-    <>
-      <Helmet>
-        <link rel="canonical" href="http://spa.mock/" />
-      </Helmet>
+    <div className="home">
       <ReactSpringWrapper />
-    </>
+    </div>
   )
 }
